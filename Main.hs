4a0d6ddcb29b306fd93d7c4749c98c4d54616ce0
kristian@purestyle.se <kristian@purestyle.se>
2015-07-12 16:25:53 +0200

    communication works

--- Main.hs
+++ Main.hs
@@ -1,21 +1,18 @@
-module Main where
-
+import Control.Monad
 import System.IO
 
-main :: IO ()
+test :: String -> String
+test inp | head (words inp) == "action" = "drop"
+         | otherwise = "no_moves"
+
+loop :: IO ()
+loop =
+    do line <- getLine
+       putStrLn $ test line
+       hFlush stdout
+       eof  <- isEOF
+       unless eof loop
+
 main =
-    do done <- isEOF
-       case done of
-           True -> return ()
-           False -> do
-               x <- getLine
-               case (head (words x)) of
-                   "settings" ->
-                       main
-                   "update" ->
-                       main
-                   "action" ->
-                       putStrLn "drop" >>
-                       main
-                   _ ->
-                       return ()
+    do hSetBuffering stdin LineBuffering
+       loop
