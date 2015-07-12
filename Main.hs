073e0a76e48f24b63fb161be47a31c5562c47278
kristian@purestyle.se <kristian@purestyle.se>
2015-07-12 19:58:55 +0200

    more moving

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ import Control.Monad
 import System.IO
 
 test :: String -> String
-test inp | head (words inp) == "action" = "left"
+test inp | head (words inp) == "action" = "left, left, down, left"
          | otherwise = "no_moves"
 
 loop :: IO ()
