05ca9dd98e91c7a3bd1b7d313676a52ba0820560
kristian@purestyle.se <kristian@purestyle.se>
2015-07-12 13:46:59 +0200

    attempt

--- Main.hs
+++ Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import System.IO
+
 main :: IO ()
-main = putStrLn "hej"
+main =
+    do done <- isEOF
+       case done of
+           True -> return ()
+           False -> do
+               x <- getLine
+               case (head (words x)) of
+                   "settings" ->
+                       main
+                   "update" ->
+                       main
+                   "action" ->
+                       putStrLn "drop" >>
+                       main
+                   _ ->
+                       return ()
