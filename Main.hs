95a1a4c765bbee94a6d53c4f5cbda9ab5a08cb25
kristian@purestyle.se <kristian@purestyle.se>
2015-07-14 14:40:20 +0200

    using StateT monad instead of just doing everything in the IO monad

--- Main.hs
+++ Main.hs
@@ -1,18 +1,49 @@
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State.Lazy
 import System.IO
+
+{-| A Context is a StateT wrapper around the game's state,
+    allowing usage of the inner IO moand.
+-}
+type Context = StateT GameState IO
+
+data GameState = GameState {
+    playerName :: String,
+    board      :: [Int]
+}
+
+-- crude debugging flag
+debug' :: Bool
+debug' = True
+
+debug :: IO () -> Context ()
+debug x = when debug' (liftIO x)
 
 test :: String -> String
 test inp | head (words inp) == "action" = "left,left,down,right"
          | otherwise = "no_moves"
 
-loop :: IO ()
-loop =
-    do line <- getLine
-       putStrLn $ test line
-       hFlush stdout
-       eof  <- isEOF
-       unless eof loop
+modstep :: Context ()
+modstep = do
+    lastVal <- get
+    put (lastVal{ playerName = "brunhilde" })
+    return ()
 
-main =
-    do hSetBuffering stdin LineBuffering
-       loop
+loop :: Context ()
+loop = do
+    state <- get
+    line  <- (liftIO getLine)
+    liftIO (putStrLn $ test line)
+    liftIO (hFlush stdout)
+    eof <- liftIO isEOF
+    debug (putStrLn (playerName state))
+    modstep
+    unless eof loop
+
+main :: IO ()
+main = do
+    hSetBuffering stdin LineBuffering
+    evalStateT loop $ GameState{playerName = "gretchen",
+                                board      = [10]
+                               }
