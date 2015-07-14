bede960a77b3982b39a29288e38c5ce6383fa0a2
kristian@purestyle.se <kristian@purestyle.se>
2015-07-14 18:37:39 +0200

    blocks

--- Main.hs
+++ Main.hs
@@ -19,9 +19,24 @@ data Block =
     T |
     Z  deriving (Show, Read, Eq, Ord)
 
+data Player = Player {
+    playerName :: Int,
+    rowPoints  :: Int,
+    combo      :: Int,
+    field      :: [[Int]]
+}
+
 data GameState = GameState {
-    playerName :: String,
-    board      :: [Int]
+    timebank          :: Int,
+    timePerMove       :: Int,
+    players           :: [Player],
+    myBot             :: Player,
+    fieldHeight       :: Int,
+    fieldWidth        :: Int,
+    round             :: Int,
+    thisPieceType     :: Block,
+    nextPieceType     :: Block,
+    thisPiecePosition :: (Int, Int)
 }
 
 -- crude debugging flag
@@ -31,30 +46,51 @@ debug' = False
 debug :: IO () -> Context ()
 debug x = when debug' (liftIO x)
 
-test :: String -> String
-test inp | head (words inp) == "action" = "left,left,down,right"
-         | otherwise = "no_moves"
+getBlock :: Block -> [[Int]]
+getBlock I = [[0,0,0,0], [1,1,1,1], [0,0,0,0], [0,0,0,0]]
+getBlock J = [[1,0,0], [1,1,1], [0,0,0]]
+getBlock L = [[0,0,1], [1,1,1], [0,0,0]]
+getBlock O = take 2 $ repeat [1,1]
+getBlock S = [[0,1,1], [1,1,0], [0,0,0]]
+getBlock T = [[0,1,0], [1,1,1], [0,0,0]]
+getBlock Z = [[1,1,0], [0,1,1], [0,0,0]]
 
 modstep :: Context ()
 modstep = do
     lastVal <- get
-    put (lastVal{ playerName = "brunhilde" })
+    put $ lastVal{ timebank = 10000 }
     return ()
+
+parse :: String -> Context ()
+parse str | head (words str) == "action"   = handleAction   str
+          | head (words str) == "update"   = handleUpdate   str
+          | head (words str) == "settings" = handleSettings str
+
+{-| Handle the action given by the admin script!
+    Make use of already set game state.
+-}
+handleAction :: String -> Context()
+handleAction str = do
+    liftIO $ putStrLn "left,left,down,right"
+
+handleSettings :: String -> Context ()
+handleSettings str = return ()
+
+handleUpdate :: String -> Context ()
+handleUpdate str = return ()
 
 loop :: Context ()
 loop = do
     state <- get
     line  <- (liftIO getLine)
-    liftIO (putStrLn $ test line)
+    parse line
     liftIO (hFlush stdout)
     eof <- liftIO isEOF
-    debug (putStrLn (playerName state))
+    debug $ putStrLn $ show $ timebank state
     modstep
     unless eof loop
 
 main :: IO ()
 main = do
     hSetBuffering stdin LineBuffering
-    evalStateT loop $ GameState{playerName = "gretchen",
-                                board      = [10]
-                               }
+    evalStateT loop $ GameState{}
